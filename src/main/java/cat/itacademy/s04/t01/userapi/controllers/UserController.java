1e3b8f1ea7ad5a9ec9f6bda6586156ee04704170
PauG <paugaston@hotmail.com>
2025-12-10 11:25:23 +0100

    feat: implementing PUT endpoint insertUser.

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,19 +1,32 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.models.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private static List<User> userList = new ArrayList<>();
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getUsers(){
         return userList;
+    }
+
+    @PostMapping
+    public User insertUser(@RequestBody User userRequest){
+        UUID id = UUID.randomUUID();
+
+        User newUser = new User(id, userRequest.getName(), userRequest.getEmail());
+
+        userList.add(newUser);
+
+        return newUser;
     }
 
 }
