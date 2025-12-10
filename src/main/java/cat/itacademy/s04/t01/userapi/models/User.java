3de595e13145a66857b659c2f91672040dc37823
PauG <paugaston@hotmail.com>
2025-12-10 11:19:22 +0100

    refactor: including id in the constructor.

--- src/main/java/cat/itacademy/s04/t01/userapi/models/User.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/models/User.java
@@ -8,7 +8,8 @@ public class User {
     private String name;
     private String email;
 
-    public User(String name, String email) {
+    public User(UUID id, String name, String email) {
+        this.id = id;
         this.name = name;
         this.email = email;
     }
