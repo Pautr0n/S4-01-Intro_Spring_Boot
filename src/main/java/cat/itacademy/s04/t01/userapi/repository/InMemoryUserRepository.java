fc060192aeecf550b1ae26a34a6f4dc663ee152a
PauG <paugaston@hotmail.com>
2025-12-11 11:15:33 +0100

    feat:  UserRepository interface and InMemoryUserRepository implemented

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/repository/InMemoryUserRepository.java
@@ -0,0 +1,44 @@
+package cat.itacademy.s04.t01.userapi.repository;
+
+import cat.itacademy.s04.t01.userapi.models.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class InMemoryUserRepository implements UserRepository {
+
+    private static List<User> userList = new ArrayList<>();
+
+    @Override
+    public User save(User user) {
+        userList.add(user);
+        return user;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return List.copyOf(userList);
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return userList.stream().filter(u->u.getId().equals(id)).findFirst();
+    }
+
+    @Override
+    public List<User> searchByName(String name) {
+        return userList.stream()
+                .filter(u->u.getName().toLowerCase().contains(name.toLowerCase()))
+                .toList();
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userList.stream()
+                .anyMatch(u->u.getEmail().equalsIgnoreCase(email));
+    }
+}
