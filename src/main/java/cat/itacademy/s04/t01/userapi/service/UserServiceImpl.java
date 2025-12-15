3a8045d56488bffeb54c6a29e26ffde46fc8b7c5
PauG <paugaston@hotmail.com>
2025-12-15 13:19:54 +0100

    feat: UserServiceImpl createUser() and findAll() implemented.

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package cat.itacademy.s04.t01.userapi.service;
+
+import cat.itacademy.s04.t01.userapi.models.User;
+import cat.itacademy.s04.t01.userapi.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User createUser(User userRequest) {
+
+        UUID id = UUID.randomUUID();
+        User newUser = new User(id, userRequest.getName(), userRequest.getEmail());
+        userRepository.save(newUser);
+
+        return newUser;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public List<User> searchByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public User findById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public User existByEmail(String email) {
+        return null;
+    }
+}
